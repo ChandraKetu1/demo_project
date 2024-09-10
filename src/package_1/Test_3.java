51382fa003f6bf980fc32c76a04139817fe41660
ChandraKetu1 <128392442+ChandraKetu1@users.noreply.github.com>
2024-09-10 22:18:57 +0530

    Update Test_3.java

--- src/package_1/Test_3.java
+++ src/package_1/Test_3.java
@@ -1,5 +1,15 @@
 package package_1;
 
 public class Test_3 {
+  public static void main(String arg []){
+    int org=123;
+    int reverse=0;
+    while(org>0){
+      int rem=org%10;
+      reverse=reverse*10+rem;
+      org=org/10;
+    }
+    System.out.println("Reverse no :"+reverse);
+  }
 
 }
