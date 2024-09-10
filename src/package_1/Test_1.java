fc626ba6878cc1ae1a4bc4dfa85ac2c11ae2a2db
chandra <chandra@DESKTOP-USAKC90>
2024-09-10 20:54:10 +0530

    first commit

--- src/package_1/Test_1.java
+++ src/package_1/Test_1.java
@@ -2,7 +2,13 @@ package package_1;
 
 public class Test_1 {
 	public static void main(String[] args) {
+		String input="chandra";
 		
+		for(int i=0;i<=input.length()-1;i++) {
+			char output=input.charAt(i);
+			System.out.println(output);
+		}
+				
 	}
 
 }
